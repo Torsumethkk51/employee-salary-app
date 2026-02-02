f9e00e0414147338e9597fe104d4af71b17c4d28
Torsumethkk51 <torsumethkkhoyo@gmail.com>
2026-02-02 23:49:54 +0700

    feat: add get user api

--- server/src/controllers/user/index.ts
+++ server/src/controllers/user/index.ts
@@ -1,6 +1,7 @@
 import Elysia from "elysia";
 import { db } from "../../database/db";
 import { user } from "../../database/schema";
+import { eq } from "drizzle-orm";
 
 export const userControllers = new Elysia({ prefix: "/users" })
 
@@ -9,6 +10,38 @@ export const userControllers = new Elysia({ prefix: "/users" })
     const result = await db
       .select()
       .from(user)
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    set.status = 500;
+    return {
+      message: "Internal server error"
+    }
+  }
+})
+
+.get("/:id", async ({ params, set }) => {
+  try {
+    const userId = Number(params.id);
+    if (Number.isNaN(userId)) {
+      set.status = 404;
+      return {
+        message: "Invalid user id"
+      }
+    }
+
+    const result = await db
+      .select()
+      .from(user)
+      .where(eq(user.user_id, userId))
+
+    if (result.length === 0) {
+      set.status = 404;
+      return {
+        message: "User not found"
+      }
+    }
 
     return result;
   } catch (error) {
