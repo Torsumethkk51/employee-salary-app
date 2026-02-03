c32383c48ce8cf7e9b3ad2726ec5c0a4730e7842
Torsumethkk51 <torsumethkkhoyo@gmail.com>
2026-02-04 02:18:26 +0700

    feat: add get employee api

--- server/src/controllers/employee/index.ts
+++ server/src/controllers/employee/index.ts
@@ -1,6 +1,7 @@
 import Elysia from "elysia";
 import { db } from "../../database/db";
 import { employee } from "../../database/schema";
+import { eq } from "drizzle-orm";
 
 export const employeeController = new Elysia({ prefix: "/employee" })
 
@@ -11,6 +12,41 @@ export const employeeController = new Elysia({ prefix: "/employee" })
       .from(employee);
 
     return result;
+  } catch (error) {
+    console.log(error)
+    set.status = 500;
+    return {
+      message: "Internal server error"
+    }
+  }
+})
+
+.get("/:id", async ({ params, set }) => {
+  try {
+    // validate employee id
+    const employeeId = Number(params.id);
+    if (!Number.isInteger(employeeId) || employeeId <= 0) {
+      set.status = 400;
+      return {
+        message: "Invalid employee id"
+      }
+    }
+
+    // fetch employee by id
+    const [employeeData] = await db
+      .select()
+      .from(employee)
+      .where(eq(employee.employee_id, employeeId))
+
+    // check if employee not found
+    if (!employeeData) {
+      set.status = 404;
+      return {
+        message: "Employee not found"
+      }
+    }
+
+    return employeeData;
   } catch (error) {
     console.log(error)
     set.status = 500;
